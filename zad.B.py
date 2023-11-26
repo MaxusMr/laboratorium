b61fc5760af02f82807fc7cc6e3dd607ca032b08
Max <maksymilian.mrozek@edu.uekat.pl>
2023-11-26 19:09:07 +0100

    poprawa2

--- zad.B.py
+++ zad.B.py
@@ -1,21 +1,21 @@
 
 
-def przez2(lista):
-    nowa_lista = []
+def multiplication(lista):
+    lista3 = []
     for element in lista:
-        nowa_lista.append(element * 2)
-    return nowa_lista
+        lista3.append(element * 2)
+    return lista3
 
 
-def przez2_skladana(lista):
+def multiplication2(lista):
     return [element * 2 for element in lista]
 
 
 lista1 = [1, 2, 3, 4, 5]
-wynik1 = przez2(lista1)
-print(wynik1)
+score1 = multiplication(lista1)
+print(score1)
 
 
 lista2 = [1, 2, 3, 4, 5]
-wynik2 = przez2_skladana(lista2)
-print(wynik2)
+score2 = multiplication2(lista2)
+print(score2)
